28a8f33df52d5ad9267ff7301f8e3823d9d451ee
scottjasontaiwan <scottjasontaiwan@yahoo.com.tw>
2024-11-28 00:59:29 +0800

    更改gini為crit,加入split終止條件,DFS

--- DecisionTree.cpp
+++ DecisionTree.cpp
@@ -31,22 +31,6 @@ void Decision_Tree::fit(MatrixXd& x, VectorXd& y, map<int, set<string>>classes, 
 	Root.values = label_counts;
 	Node* node = &Root;
 	split(node, features);
-	cout << Root.feature << "<=" << Root.threshold << endl;
-	cout << Root.criteria << endl;
-	cout << Root.samples << endl;
-	cout << "[";
-	for (auto p : Root.values) { cout << p.first << ":" << p.second << ","; }
-	cout << "\b]" << endl;
-	cout << Root.left->criteria << endl;
-	cout << Root.left->samples << endl;
-	cout << "[";
-	for (auto p : Root.left->values) { cout << p.first << ":" << p.second << ","; }
-	cout << "\b]" << endl;
-	cout << Root.right->criteria << endl;
-	cout << Root.right->samples << endl;
-	cout << "[";
-	for (auto p : Root.right->values) { cout << p.first << ":" << p.second << ","; }
-	cout << "\b]" << endl;
 }
 
 map<int,int> Decision_Tree::label_count(VectorXd& data) {
@@ -75,7 +59,7 @@ double Decision_Tree::calc_entropy(VectorXd& data, map<int, int>label_counts) {
 	return entropy;
 }
 
-double Decision_Tree::threshold(VectorXd& x, VectorXd& y, int col, double gini, vector<string>features, Node* node) {
+double Decision_Tree::threshold(VectorXd& x, VectorXd& y, int col, double crit, vector<string>features, Node* node) {
 	double thres;
 	sort(x.data(), x.data() + x.size());
 	for (int i = 0; i < x.size(); i++) {
@@ -96,44 +80,68 @@ double Decision_Tree::threshold(VectorXd& x, VectorXd& y, int col, double gini, 
 				data_l.row(data_l.rows() - 1) = node->data.row(r);
 			}
 		}
-		double gini_sub = 0, gini_l = 0, gini_r = 0;
+		double crit_sub = 0, crit_l = 0, crit_r = 0;
 		map<int, int>label_l, label_r;
 		if (data_l.cols() > 0) {
 			VectorXd y_l = data_l.col(data_l.cols() - 1);
 			label_l = label_count(y_l);
-			gini_l = gini_impurity(y_l,label_l);
-			gini_sub += (double)y_l.size() / node->data.rows() * gini_l;
+			crit_l = (criterion == "gini") ? gini_impurity(y_l,label_l) : calc_entropy(y_l,label_l);
+			crit_sub += (double)y_l.size() / node->data.rows() * crit_l;
 		}
 		if (data_r.cols() > 0) {
 			VectorXd y_r = data_r.col(data_r.cols() - 1);
 			label_r = label_count(y_r);
-			gini_r = gini_impurity(y_r,label_r);
-			gini_sub += (double)y_r.size() / node->data.rows() * gini_r;
+			crit_r = (criterion == "gini") ? gini_impurity(y_r,label_r) : calc_entropy(y_r,label_r);
+			crit_sub += (double)y_r.size() / node->data.rows() * crit_r;
 		}
-		if (gini_sub < gini) {
-			gini = gini_sub;
+		if (crit_sub < crit) {
+			crit = crit_sub;
 			node->threshold = thres;
 			node->feature = features[col];
 			node->left = new Node();
 			node->right = new Node();
 			node->left->data = data_l;
-			node->left->criteria = gini_l;
+			node->left->criteria = crit_l;
 			node->left->samples = data_l.rows();
 			node->left->values = label_l;
 			node->right->data = data_r;
-			node->right->criteria = gini_r;
+			node->right->criteria = crit_r;
 			node->right->samples = data_r.rows();
 			node->right->values = label_r;
 		}
 	}
-	return gini;
+	return crit;
 }
 
 void Decision_Tree::split(Node* node, vector<string>features) {
-	double gini_split = 1;
+	cout << "------------------------------------" << endl;
+	if (node->isLeaf) { return; }
+	double crit_split = 1;
 	VectorXd y = node->data.col(node->data.cols() - 1);
 	for (int i = 0; i < node->data.cols() - 1; i++) {
 		VectorXd data_i = node->data.col(i);
-		gini_split = threshold(data_i, y, i, gini_split, features, node);
+		crit_split = threshold(data_i, y, i, crit_split, features, node);
 	}
+	if (node->left->criteria == 0) { node->left->isLeaf = true; }
+	if (node->right->criteria == 0) { node->right->isLeaf = true; }
+
+	cout << "Split threashold : " << node->feature << "<=" << node->threshold << endl;
+	cout << "node criteria = " << node->criteria << endl;
+	cout << "node sample size = " << node->samples << endl;
+	cout << "node label count : [";
+	for (auto p : node->values) { cout << p.first << ":" << p.second << ","; }
+	cout << "\b]" << endl;
+	cout << "left criteria = " << node->left->criteria << endl;
+	cout << "left sample size = " << node->left->samples << endl;
+	cout << "left label count : [";
+	for (auto p : node->left->values) { cout << p.first << ":" << p.second << ","; }
+	cout << "\b]" << endl;
+	cout << "right criteria = " << node->right->criteria << endl;
+	cout << "right sample size = " << node->right->samples << endl;
+	cout << "right label count : [";
+	for (auto p : node->right->values) { cout << p.first << ":" << p.second << ","; }
+	cout << "\b]" << endl;
+
+	split(node->left, features);
+	split(node->right, features);
 }
